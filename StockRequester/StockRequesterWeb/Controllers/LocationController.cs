3edfbb586b589f85a0275e284e1e1d20daaa08eb
samluddite <56632175+SamsRepos@users.noreply.github.com>
2023-12-30 07:03:07 +0000

    Added basic locations index

---
+++ StockRequester/StockRequesterWeb/Controllers/LocationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockRequester.DataAccess.Repository.IRepository;
+using StockRequester.Models;
+
+namespace StockRequesterWeb.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LocationController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index(int? companyId)
+        {
+            List<Location> allLocationsList = _unitOfWork.Location.GetAll(includeProperties:"Company").ToList();
+
+            List<Location> locationsList = allLocationsList.Where(u => u.CompanyId == companyId).ToList();
+            
+            return View(locationsList);
+        }
+    }
+}
